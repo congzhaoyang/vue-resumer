0952a9967d244d7881d1852572a3b4b10794f29f
congzhaoyang <congzhaoyang1@163.com>
2017-08-07 21:37:07 +0800

    显示用户名和登出

--- src/components/Topbar.vue
+++ src/components/Topbar.vue
@@ -1,15 +1,27 @@
 <template>
     <div id="topbar">
-        <div class="h1">topbar</div>
-        <span>{{ user }}</span>
-        <a class="button primary" href="#" @click.prevent="signUpDialogVisible = true">注册</a>
-        <MyDialog title="注册" :visible="signUpDialogVisible" @close="signUpDialogVisible = false">
-          <SignUpForm @success="login($event)"/>
-        </MyDialog>
-        <a class="button" href="#">登录</a>
-        <div class="button">
-            <el-button type="primary"> &nbsp&nbsp保存&nbsp&nbsp</el-button>
-            <el-button type="primary"> &nbsp&nbsp预览&nbsp&nbsp </el-button>
+        <div class="wrapper">
+            <span class="logo">resumer</span>
+            <div class="actions">
+                <div v-if="logined" class="userActions">
+                    <span>你好，{{user.username}}</span>
+                    <a class="button" href="#" @click.prevent="signOut">登出</a>
+                    <el-button type="primary"@click.prevent="signOut"> &nbsp&nbsp登出&nbsp&nbsp</el-button>
+                </div>
+                <div v-else class="userActions">
+                    <a class="button primary" hrefd="#" @click.prevent="signUpDialogVisible = true">注册</a>
+                    <el-button type="primary" @click.prevent="signUpDialogVisible = true"> &nbsp&nbsp注册&nbsp&nbsp</el-button>
+                    <MyDialog title="注册" :visible="signUpDialogVisible" @close="signUpDialogVisible = false">
+                        <SignUpForm @success="signIn($event)"/>
+                    </MyDialog>
+                    <a class="button" href="#">登录</a>
+                    <el-button type="primary"> &nbsp&nbsp登录&nbsp&nbsp</el-button>
+                </div>
+                <div class="element-button">
+                    <el-button type="primary"> &nbsp&nbsp保存&nbsp&nbsp</el-button>
+                    <el-button type="primary"> &nbsp&nbsp预览&nbsp&nbsp </el-button>
+                </div>
+            </div>
         </div>
     </div>
 </template>
@@ -17,6 +29,7 @@
 <script>
 import MyDialog from './MyDialog'
 import SignUpForm from './SignUpForm'
+import AV from '../lib/leancloud'
 export default {
     name: 'Topbar',
     data(){
@@ -27,13 +40,20 @@ export default {
     computed: {
         user() {
             return this.$store.state.user
+        },
+        logind() {
+            return this.user.id
         }
     },
     components: {
         MyDialog, SignUpForm
     },
     mothods: {
-        login(user) {
+        signOut(user) {
+            AV.user.logOut()
+            this.$store.commit('removeUser')
+        },
+        signIn(user) {
             this.signUpDialogVisible = false
             this.$store.commit('setUser', user)
         }
@@ -43,19 +63,38 @@ export default {
 
 <style scoped lang="scss">
 #topbar {
+    height: 64px;
     width: 1220px;
-    background: white;
-    line-height: 64px;
-    border-radius: 4px;
-    display: flex;
-    flex-flow: row nowrap;
-    justify-content: space-between;
-    .h1 {
-        width: 100px;
-    }
-    .button {
-        margin-right: 20px;
-        width: 166px;
+    .wrapper {
+        border: 1px solid red;
+        width: 100%;
+        display: flex;
+        justify-content: space-around;
+        .logo {
+            display: block;
+            width: 10%;
+            border: 1px solid blue;
+            height: 64px;
+            line-height: 64px;
+        }
+        .actions {
+            .user-action {
+                display: flex;
+                align-items: center;
+            }
+            border: 1px solid purple;
+            display: flex;
+            justify-content: space-between;
+            width: 90%;
+            height: 64px;
+            .element-button {
+                margin-right: 20px;
+                width: 166px;
+                border: 1px solid green;
+                display: flex;
+                align-items: center;
+            }
+        }
     }
 }
 </style>
