8658fe51c8b376f22603f26031e1c1e0580257b6
congzhaoyang <congzhaoyang1@163.com>
2017-08-07 22:13:01 +0800

    finish task9

--- src/components/Topbar.vue
+++ src/components/Topbar.vue
@@ -1,105 +1,120 @@
 <template>
-    <div id="topbar">
-        <div class="wrapper">
-            <span class="logo">resumer</span>
-            <div class="actions">
-                <div v-if="logined" class="userActions">
-                    <span>你好，{{user.username}}</span>
-                    <a class="button" href="#" @click.prevent="signOut">登出</a>
-                    <el-button type="primary" @click.prevent="signOut"> &nbsp&nbsp登出&nbsp&nbsp</el-button>
-                </div>
-                <div v-else class="userActions">
-                    <a class="button primary" hrefd="#" @click.prevent="signUpDialogVisible = true">注册</a>
-                    <el-button type="primary" @click.prevent="signUpDialogVisible = true"> &nbsp&nbsp注册&nbsp&nbsp</el-button>
-                    <MyDialog title="注册" :visible="signUpDialogVisible" @close="signUpDialogVisible = false">
-                        <SignUpForm @success="signIn($event)"/>
-                    </MyDialog>
-                    <a class="button" href="#">登录</a>
-                    <el-button type="primary"> &nbsp&nbsp登录&nbsp&nbsp</el-button>
-                </div>
-                <div class="element-button">
-                    <el-button type="primary"> &nbsp&nbsp保存&nbsp&nbsp</el-button>
-                    <el-button type="primary"> &nbsp&nbsp预览&nbsp&nbsp </el-button>
-                </div>
-            </div>
+  <div id="topbar">
+    <div class="wrapper">
+      <span class="logo">Resumer</span>
+
+      <div class="actions">
+        <div v-if="logined" class="userActions">
+          <span class="welcome">你好，{{user.username}}</span>
+          <a class="button" href="#" @click.prevent="signOut">登出</a>
         </div>
+        <div v-else class="userActions">
+          <a class="button primary" href="#" @click.prevent="signUpDialogVisible = true">注册</a>
+          <MyDialog title="注册" :visible="signUpDialogVisible" @close="signUpDialogVisible = false">
+            <SignUpForm @success="signIn($event)"/>
+          </MyDialog>
+          <a class="button" href="#" @click.prevent="signInDialogVisible = true">登录</a>
+          <MyDialog title="登录" :visible="signInDialogVisible"
+            @close="signInDialogVisible = false">
+            <SignInForm />
+          </MyDialog>
+        </div>
+        <button class="button primary">保存</button>
+        <button class="button">预览</button>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
 import MyDialog from './MyDialog'
 import SignUpForm from './SignUpForm'
+import SignInForm from './SignInForm'
 import AV from '../lib/leancloud'
 export default {
-    name: 'Topbar',
-    data(){
-        return {
-        signUpDialogVisible: false
-        }
-    },
-    computed: {
-        user() {
-            return this.$store.state.user
-        },
-        logind() {
-            return this.user.id
-        }
-    },
-    components: {
-        MyDialog, SignUpForm
-    },
-    mothods: {
-        signOut(user) {
-            AV.user.logOut()
-            this.$store.commit('removeUser')
-        },
-        signIn(user) {
-            this.signUpDialogVisible = false
-            this.$store.commit('setUser', user)
-        }
+  name: 'Topbar',
+  data(){
+    return {
+      signUpDialogVisible: false,
+      signInDialogVisible: false,
     }
+  },
+  computed: {
+    user(){
+      return this.$store.state.user
+    },
+    logined(){
+      return this.user.id
+    }
+  },
+  components: {
+    MyDialog, SignUpForm, SignInForm
+  },
+  methods: {
+    signOut(){
+      AV.User.logOut()
+      this.$store.commit('removeUser')
+    },
+    signIn(user){
+      this.signUpDialogVisible = false
+      this.$store.commit('setUser', user)
+    }
+  }
 }
 </script>
 
 <style scoped lang="scss">
-#topbar {
-    height: 64px;
-    width: 1220px;
-    .wrapper {
-        border: 1px solid red;
-        width: 100%;
-        display: flex;
-        justify-content: space-around;
-        .logo {
-            display: block;
-            width: 10%;
-            border: 1px solid blue;
-            height: 64px;
-            line-height: 64px;
-        }
-        .actions {
-            .user-action {
-                display: flex;
-                align-items: center;
-            }
-            border: 1px solid purple;
-            display: flex;
-            justify-content: space-between;
-            width: 90%;
-            height: 64px;
-            .element-button {
-                margin-right: 20px;
-                width: 166px;
-                border: 1px solid green;
-                display: flex;
-                align-items: center;
-            }
-        }
+  // 自行查阅 scoped 的功能
+  // 见：https://cn.vuejs.org/v2/guide/comparison.html#CSS-的组件作用域
+  #topbar{
+    border-radius: 4px;
+    background:#ffffff;
+    box-shadow:0 1px 3px 0 rgba(0,0,0,0.25);
+    >.wrapper{
+      min-width: 1024px;
+      max-width: 1440px;
+      margin: 0 auto;
+      height:64px;
     }
-}
+    >.wrapper{
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      padding: 0 16px;
+    }
+    .logo{
+      font-size:24px;
+      color:#000000;
+    }
+  }
+  .button{ // 由于加了 scoped， 所以这个 button 选择器只在本组件内有效，不会影响其他组件
+    width:72px;
+    height:32px;
+    border: none;
+    cursor: pointer;
+    font-size: 18px; // 设计稿上是 20px，看起来太大，就改成 18px 了
+    background:#ddd;
+    color: #222;
+    text-decoration: none;
+    display: inline-flex;
+    justify-content: center;
+    align-items: center;
+    vertical-align: middle;
+    &:hover{
+      box-shadow: 1px 1px 1px hsla(0, 0, 0, 0.50);
+    }
+    &.primary{
+      background:#02af5f;
+      color: white;
+    }
+  }
+  .actions{
+    display: flex;
+    .userActions{
+      margin-right: 3em;
+      .welcome{
+        margin-right: .5em;
+      }
+    }
+  }
 </style>
-
-
-
-
-
